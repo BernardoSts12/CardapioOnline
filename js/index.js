50867250958805eac733e7e0e04681194c855799
BernardoSts12 <bernardosilvasilver@gmail.com>
2022-05-14 13:09:49 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// Slick Slider
+$(document).ready(function() {
+    $('.carousel-slick').slick({
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        swipeToSlid: true,
+        variableWidth: true,
+
+    });
+
+});
